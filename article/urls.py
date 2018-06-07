e29a231b41a673d068e78b5f230a06ba0cc43b2f
Andrey <11446999+yrdnv@users.noreply.github.com>
2018-06-07 17:34:36 +0300

    Update urls.py

--- article/urls.py
+++ article/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('<pk>', views.DetailArticleView.as_view(), name='article')
-    path('<pk>', views.article, name='article')
+    path('<pk>/', views.article, name='article')
 
 ]
